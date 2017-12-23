fa975e454767d937d96b4589074ce79f40293038
vagrant <vagrant@homestead>
2017-12-23 04:07:30 +0000

    增加邮件激活用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = 'abc@abc.com';
         $user->password = bcrypt('123456');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
